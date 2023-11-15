4c5451cc732bbee32030574a42ce487a3e227f76
Arthur <pe.cker@yandex.ru>
2023-11-15 20:11:36 +0300

    in user_desk_insert added  other users from wor_pl

--- user_desk_insertion.py
+++ user_desk_insertion.py
@@ -1,16 +1,28 @@
-def user_desk_insertion(connection, number_desks): # number_workplaces - количество worklaces, привязанные к user'у
-    cur = connection.cursor()   # копируем записи из desk
+def user_desk_insertion(connection):
+    cur = connection.cursor() 
     
     cur.execute(
-        f"SELECT desk_id, admin_id FROM Desk;"
+        f"SELECT desk_id, admin_id, work_place_id FROM Desk;"
     )
-    all_desks = list(cur.fetchall())
+    all_desks_admins_workplaces = list(cur.fetchall())
     
-    for desk_id, admin_id in all_desks:
+    
+    cur.execute(
+        f"SELECT user_id, work_place_id FROM User_work_place;"
+    )
+    all_users_workplaces = list(cur.fetchall())
+    
+    for desk_id, admin_id, workplace_id1 in all_desks_admins_workplaces:
         cur.execute(
             f"INSERT INTO User_Desk (user_id, desk_id) "
             f"VALUES ({admin_id}, {desk_id})"
         )
+        for user_id, workplace_id2 in all_users_workplaces:
+            if workplace_id1 == workplace_id2:
+                 cur.execute(
+                    f"INSERT INTO User_Desk (user_id, desk_id) "
+                    f"VALUES ({user_id}, {desk_id})"
+                )
 
     connection.commit()
     print("User_desks were added successfully!")
