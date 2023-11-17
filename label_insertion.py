52825264171f897b75783e1f3bc609cc19d42fed
Arthur <pe.cker@yandex.ru>
2023-11-18 00:04:18 +0300

    Sasha made changes in database and label_insertion

--- label_insertion.py
+++ label_insertion.py
@@ -2,7 +2,7 @@ import random
 import string
 
 from database import DESK_BACKGROUND_LIST
-
+from database import LABEL_TEXT_LIST
 
 def label_insertion(connection, number):
     cur = connection.cursor()
@@ -12,13 +12,10 @@ def label_insertion(connection, number):
     )
     all_workplaces = list(map(lambda item: item[0], cur.fetchall()))
     
-    colour = random.choice(DESK_BACKGROUND_LIST)
-
-    characters = string.ascii_letters + string.digits
-    text = ''.join(random.choice(characters) for _ in range(random.randint(1, 10)))
-
     available_workplaces = set(all_workplaces)
     for _ in range(number):
+        colour = random.choice(DESK_BACKGROUND_LIST)
+        text = random.choice(LABEL_TEXT_LIST)
         to_what_workplace = random.choice(list(available_workplaces))
         available_workplaces.remove(to_what_workplace)
         cur.execute(
